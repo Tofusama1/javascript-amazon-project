605f2949789e48af051c014688cc990d861a4a80
Chris <Christan8@yahoo.com>
2024-08-08 00:10:11 -0400

    Delete products from cart when clicking delete

--- data/cart.js
+++ data/cart.js
@@ -1,5 +1,5 @@
 //export can be used outside of cart.js
-export const cart = [{
+export let cart = [{
     //you don't need to copy everything like the image and the price, you can normalize the data
     productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
     quantity: 2,
@@ -25,4 +25,15 @@ export function addToCart(productId) {
                   quantity: 1
               });
           }
+}
+
+ export function removeFromCart(productId) {
+    const newCart = [];
+
+    cart.forEach((cartItem) => {
+        if (cartItem.productId !== productId){
+            newCart.push(cartItem);
+        }
+    });
+    cart = newCart;
 }
