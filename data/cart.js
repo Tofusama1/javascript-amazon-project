b80712a52e10b0ee516eab03c61352103895a383
Chris <Christan8@yahoo.com>
2024-08-08 00:31:30 -0400

    Save cart in localStorage

--- data/cart.js
+++ data/cart.js
@@ -1,12 +1,19 @@
 //export can be used outside of cart.js
-export let cart = [{
-    //you don't need to copy everything like the image and the price, you can normalize the data
-    productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
-    quantity: 2,
-}, {
-    productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
-    quantity: 1,
-}];
+export let cart = JSON.parse(localStorage.getItem('cart'));
+if( !cart ){
+    cart = [{
+        //you don't need to copy everything like the image and the price, you can normalize the data
+        productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+        quantity: 2,
+    }, {
+        productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
+        quantity: 1,
+    }];
+}
+
+function saveToStorage(){
+    localStorage.setItem('cart', JSON.stringify(cart));
+}
 
 export function addToCart(productId) {
     let matchingItem;
@@ -25,6 +32,8 @@ export function addToCart(productId) {
                   quantity: 1
               });
           }
+    
+    saveToStorage();
 }
 
  export function removeFromCart(productId) {
@@ -36,4 +45,6 @@ export function addToCart(productId) {
         }
     });
     cart = newCart;
+
+    saveToStorage();
 }
