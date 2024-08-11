c5f41c493b9af02ed7b877f7843fada8ef0282d0
Chris <Christan8@yahoo.com>
2024-08-11 19:11:46 -0400

    Make localStorageKey and loadFromStorage private

--- data/cart-class.js
+++ data/cart-class.js
@@ -1,17 +1,17 @@
 class Cart {
     cartItems;
-    localStorageKey;
+    #localStorageKey;
 
     //More details about constructor:
     //1. It has to be named "constructor"
     //2. Should not return anything
     constructor(localStorageKey) { //when we generate an object it will automatically run this constructor great place for setup code
-        this.localStorageKey = localStorageKey;
-        this.loadFromStorage();
+        this.#localStorageKey = localStorageKey;
+        this.#loadFromStorage();
     }
 
-    loadFromStorage(){
-        this.cartItems = JSON.parse(localStorage.getItem(this.localStorageKey));
+    #loadFromStorage(){
+        this.cartItems = JSON.parse(localStorage.getItem(this.#localStorageKey));
         if( !this.cartItems ){
             this.cartItems = [{
                 //you don't need to copy everything like the image and the price, you can normalize the data
@@ -27,7 +27,7 @@ class Cart {
     }
 
     saveToStorage(){
-        localStorage.setItem(this.localStorageKey, JSON.stringify(this.cartItems));
+        localStorage.setItem(this.#localStorageKey, JSON.stringify(this.cartItems));
     }
 
     addToCart(productId, quantity) {
