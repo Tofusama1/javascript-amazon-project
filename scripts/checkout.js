72d77db5e894c76cac2e5b29c10ae30037c03135
Chris <Christan8@yahoo.com>
2024-08-12 14:51:41 -0400

    Learn promises and vonvert code to promises

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,10 +1,63 @@
 import { renderOrderSummary } from "./checkout/orderSummary.js";
 import { renderPaymentSummary } from "./checkout/paymentSummary.js";
 import { loadProducts } from "../data/products.js";
+import { loadCart } from "../data/cart.js";
 //import '../data/cart-class.js';
 //import '../data/backend-practice.js';
 
-loadProducts(() => {
+//this creates a separate thread of code but this separate thread of code doesn't actually have a next step.
+//Promise.all lets us run multiple promises at the same time and waits for all of them to finish
+
+Promise.all([ //Array of promises
+    new Promise((resolve) => { //runs the function immediately
+        //console.log('start promise');
+        loadProducts(() => {
+            //console.log('finish loading')
+            //whatever we give to resolve will be saved in the parameter value (.then((value));)
+            resolve('value1');
+        });
+    }),
+    new Promise((resolve) => {
+        loadCart(() => {
+            resolve();
+        });
+    })
+
+]).then((values) => {
+    console.log(values);
     renderOrderSummary();
     renderPaymentSummary();
 });
+
+/*
+new Promise((resolve) => { //runs the function immediately
+    //console.log('start promise');
+    loadProducts(() => {
+        //console.log('finish loading')
+        //whatever we give to resolve will be saved in the parameter value (.then((value));)
+        resolve('value1');
+    });
+}).then((value) => { //this is the next step
+    //console.log('next step');
+    console.log(value);
+    return new Promise((resolve) => {
+        loadCart(() => {
+            resolve();
+        });
+    });
+}).then(() => {
+    renderOrderSummary();
+    renderPaymentSummary();
+}); */
+
+//this is running at the same time as the code above
+//This allows JavaScript to do multiple things at the same time
+/*
+loadProducts(() => {
+    loadCart(() => {
+        renderOrderSummary();
+        renderPaymentSummary();
+    })
+}); */
+
+/* If we have a lot of callbacks, out code will become more and more nested, multiple callbacks cause a lot of nesting making it harder to work with*/
