f2ef1012906104828fd723cde94e2c7f27289b0b
Chris <Christan8@yahoo.com>
2024-08-12 13:25:05 -0400

    add Backend practice file

---
+++ data/backend-practice.js
@@ -0,0 +1,11 @@
+const xhr = new XMLHttpRequest();
+
+xhr.addEventListener('load', () => {
+    console.log(xhr.response);
+});
+//Get = get some information from the backend
+//Types of requests: Get Post Put Delete
+//xhr.open takes in 2 parameter: the first is the type of request the second is where to send this http message by using URL
+//Uniform Resource Locator
+xhr.open('GET', 'https://supersimplebackend.dev/images/apple.jpg');
+xhr.send();
