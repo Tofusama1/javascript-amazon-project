da9b258d75258ad240cd83d88305cce751040c5b
Chris <Christan8@yahoo.com>
2024-08-11 21:39:46 -0400

    convert products into classes

--- data/products.js
+++ data/products.js
@@ -1,3 +1,5 @@
+import formatCurrency from "../scripts/utils/money.js";
+
 export function getProduct(productId) {
   let matchingProduct;
   products.forEach((product) => {
@@ -7,6 +9,45 @@ export function getProduct(productId) {
   });
   return matchingProduct;
 }
+class Product {
+  id;
+  image;
+  name;
+  priceCents;
+
+  constructor(productDetails) {
+    this.id = productDetails.id;
+    this.image = productDetails.image;
+    this.name = productDetails.name;
+    this.rating = productDetails.rating;
+    this.priceCents = productDetails.priceCents;
+  }
+
+  getStarsUrl(){
+    return `images/ratings/rating-${this.rating.stars * 10}.png`;
+  }
+
+  getPrice(){
+    return `$${formatCurrency(this.priceCents)}`;
+  }
+}
+
+/*const product1 = new Product({
+  id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+  image: "images/products/athletic-cotton-socks-6-pairs.jpg",
+  name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
+  rating: {
+    stars: 4.5,
+    count: 87
+  },
+  priceCents: 1090,
+  keywords: [
+    "socks",
+    "sports",
+    "apparel"
+  ]
+});
+console.log(product1);*/
 
 export const products = [
   {
@@ -667,4 +708,15 @@ export const products = [
       "mens"
     ]
   }
-];
+].map((productDetails) => {
+  //we are transforming this object into a class
+  return new Product(productDetails);
+});
+
+console.log(products);
+
+
+//map loops through an array and for each value it runs a function
+
+//how map works is that it will run this function on it and we transform it and then put it inside a new array
+
