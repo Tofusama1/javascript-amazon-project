14ed0f7043d5b674ed354ee585aa80fd033f9952
Chris <Christan8@yahoo.com>
2024-08-12 22:40:21 -0400

    Create order using the backend

--- data/backend-practice.js
+++ data/backend-practice.js
@@ -4,6 +4,9 @@ xhr.addEventListener('load', () => {
     console.log(xhr.response);
 });
 //Get = get some information from the backend
+//Post = create something
+//Put = update something
+//Delete = delete something
 //Types of requests: Get Post Put Delete
 //xhr.open takes in 2 parameter: the first is the type of request the second is where to send this http message by using URL
 //Uniform Resource Locator
--- scripts/checkout/paymentSummary.js
+++ scripts/checkout/paymentSummary.js
@@ -2,6 +2,7 @@ import { cart } from "../../data/cart.js";
 import { getDeliveryOption } from "../../data/deliveryOptions.js";
 import { getProduct } from "../../data/products.js";
 import formatCurrency from "../utils/money.js";
+import { addOrder } from "../../data/orders.js";
 
 
 export function renderPaymentSummary(){
@@ -65,11 +66,39 @@ export function renderPaymentSummary(){
             </div>
         </div>
 
-        <button class="place-order-button button-primary">
+        <button class="place-order-button button-primary js-place-order">
         Place your order
         </button>
     `;
 
     document.querySelector('.js-payment-summary').innerHTML = paymentSummaryHTML;
 
+    document.querySelector('.js-place-order').addEventListener('click', async () => {
+        try{
+        //await = wait for fetch to finish
+        const response = await fetch('https://supersimplebackend.dev/orders', {
+            method: 'POST',
+            //headers gives the backend more information about our request
+            headers: {
+                //this tells the backend what type of data we're sending in our request
+                'Content-Type': 'application/json'
+            },
+            //the actual data we are sending to the backend and we are converting it to a json
+            body: JSON.stringify({
+                cart: cart
+            })
+        });
+
+        //response.json is a promise
+        const order = await response.json();
+        addOrder(order);
+
+    } catch (error) {
+        console.log('Unexpected error. Try again later.');
+    }
+
+    //lets us control the URL at the top of the page, if we change the location object, it will change the url at the top 
+    //href gives us the URL at the top of the browser, if we change the href property it will change the url at the top of the browser
+    window.location.href = 'orders.html';
+    });
 }
